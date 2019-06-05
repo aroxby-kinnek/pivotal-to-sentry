444d79f7d306413842bf67c057e8a91de2cb6776
Andy Roxby <aroxby@kinnek.com>
2019-06-05 17:58:00 -0400

    Add some setup and start main program

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+The purpose of this module is to find all Pivotal stories tagged with 'sentry'
+and report those stories 'accepted' on Pivotal but activate on Sentry.
+"""
+from pivotal_to_sentry.pivotal import PivotalClient
+
+
+def main():
+    pivotal = PivotalClient()
+    project_id = pivotal.get_all_projects()[0]['id']
+    stories = pivotal.get_all_stories(project_id, with_label='sentry')
+    for story in stories:
+        print u'{}: {}'.format(story['id'], story['name'])
+
+
+if __name__ == '__main__':
+    main()
