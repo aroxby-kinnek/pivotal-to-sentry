12de5e90a1b0d2b9c557ef7792607f7100da23d0
Andy Roxby <aroxby@kinnek.com>
2019-06-06 11:51:03 -0400

    Implement some Sentry stuff

--- pivotal_to_sentry/sentry.py
+++ pivotal_to_sentry/sentry.py
@@ -1,9 +1,41 @@
 import os
+import re
 from pivotal_to_sentry.rest import RestClient
 
 
 class SentryClient(RestClient):
     base_url = 'https://sentry.io/api/0/'
+
+    class PageLink(object):
+        def __init__(self, link, rel, results, cursor):
+            self.link = link
+            self.rel = rel
+            self.results = results.lower() == 'true'
+            self.cursor = cursor
+
+        @classmethod
+        def parse_link_header(cls, header):
+            regex = (
+                '^'
+                r'<(?P<link>[^>]+)>'
+                r';\s*rel="(?P<rel>[^"]+)"'
+                r';\s*results="(?P<results>[^"]+)"'
+                r';\s*cursor="(?P<cursor>[^"]+)"'
+                '$'
+            )
+            links = [
+                part.strip() for part in header.split(',') if part.strip()
+            ]
+            cursors = []
+            while links:
+                link = links[0]
+                links = links[1:]
+                match = re.match(regex, link)
+                if not match:
+                    raise ValueError('Malformed link header')
+                cursor = cls(**match.groupdict())
+                cursors.append(cursor)
+            return cursors
 
     def _get_auth_token(self):
         token = os.environ.get('SENTRY_TOKEN')
@@ -19,3 +51,29 @@ class SentryClient(RestClient):
         headers['Authorization'] = 'Bearer {}'.format(self.auth_token)
         return params, data, headers
 
+    def paged_json_request(self, url, params=None, data=None, headers=None):
+        while url:
+            response = self.request(url, params, data, headers)
+            link_header = response.headers.get('Link', '')
+            links = self.PageLink.parse_link_header(link_header)
+            named_links = {
+                link.rel: link.link for link in links if link.results
+            }
+            url = named_links.get('next')
+            if url:  # Bit of a hack
+                url = url.replace(self.base_url, '')
+            yield response.json()
+
+    def all_pages_json(self, url, params=None, data=None, headers=None):
+        lst = []
+        for page in self.paged_json_request(url, params, data, headers):
+            lst += page
+        return lst
+
+    def get_projects(self):
+        url = 'projects/'
+        return self.all_pages_json(url)
+
+    def get_issues(self, organization, project):
+        url = 'projects/{}/{}/issues/'.format(organization, project)
+        return self.all_pages_json(url)
